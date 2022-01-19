0dc688069211d59b7f4e218093cfab0c7a4d9106
zigaz <zigaz@DESKTOP-UT9VTF9>
2022-01-19 20:23:11 +0300

    Add project files.

---
+++ домашняя работа упр1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace домашняя_работа_упр1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");// нашел гарячие клавиши ctrl + 5 9 ставится точка с запятой
+        }
+    }
+}
